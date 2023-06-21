166ac55efc836ac90410f782d6d49b7aad36c469
TakishevDarkhan <kereydarkhan@yandex.kz>
2023-06-21 08:12:04 +0600

    add gitignore

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,23 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+
+
+class Node(unittest.TestCase):
+    def test_init(self):
+        self.assertNotEqual((5, None), 5)
+        self.assertNotEqual(('a', 'n1'), 'a')
+        self.assertTrue('n1', '<src.stack.Node object at 0x0000024D3A439150>')
+        self.assertTrue('n2.next_node', '<__main__.Node object at 0x0000022803036050>')
+
+class Stack(unittest.TestCase):
+    def test_push(self):
+        self.assertNotEqual(('data2', 'data3'), 'data1')
+        self.assertNotEqual(('data1', 'data3'), 'data2')
+        self.assertNotEqual(('data2', 'data1'), 'data3')
+
+    def test_pop(self):
+        self.assertIsNone(None)
+        self.assertTrue("AttributeError: 'NoneType' object has no attribute 'data'")
+
+
+
